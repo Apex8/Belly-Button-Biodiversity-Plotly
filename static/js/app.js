20d379406ddda6fb124b177a9fce1968b3afe486
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-09-28 18:46:08 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -14,8 +14,54 @@ function init(){
     updatemetadata(firstbutton);
     });
 }
-
-function updateMetadata(sample) {
+  
+  function updateCharts(sample) {    
+    d3.json("data/samples.json").then((data) => {
+    var samples = data.samples;
+    var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
+    var result = filterArray[0];
+    var sample_values = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;   
+    var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale:"Electric"
+        }
+    };
+    var data = [trace1];
+    var layout = {
+        title: 'Bacteria Cultures per Sample',
+        showlegend: false,
+        hovermode: 'closest',
+        xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
+        font: { color: "#49a81d", family: "Arial, Helvetica, sans-serif" },
+        margin: {t:30}
+    };
+    Plotly.newPlot('bubble', data, layout); 
+    var trace1 = {
+        x: sample_values.slice(0,10).reverse(),
+        y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+        text: otu_labels.slice(0,10).reverse(),
+        name: "Greek",
+        type: "bar",
+        orientation: "h"
+    };
+    var data = [trace1];
+    var layout = {
+        title: "Top Ten OTUs for Individual " +sample,
+        margin: {l: 100, r: 100, t: 100, b: 100}
+    };
+    Plotly.newPlot("bar", data, layout);  
+    });
+  }
+  
+  function updateMetadata(sample) {
     d3.json("data/samples.json").then((data) => {
         var metadata = data.metadata;
         var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
@@ -53,56 +99,9 @@ function updateMetadata(sample) {
     });
   }
   
-  function updateCharts(sample) {    
-    d3.json("data/samples.json").then((data) => {
-    var samples = data.samples;
-    var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
-    var result = filterArray[0];
-    var sample_values = result.sample_values;
-    var otu_ids = result.otu_ids;
-    var otu_labels = result.otu_labels;   
-    var trace1 = {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
-        mode: 'markers',
-        marker: {
-        size: sample_values,
-        color: otu_ids,
-        colorscale:"Electric"
-        }
-    };
-    var data = [trace1];
-    var layout = {
-        title: 'Bacteria Cultures per Sample',
-        showlegend: false,
-        hovermode: 'closest',
-        xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
-        margin: {t:30}
-    };
-    Plotly.newPlot('bubble', data, layout); 
-    var trace1 = {
-        x: sample_values.slice(0,10).reverse(),
-        y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
-        text: otu_labels.slice(0,10).reverse(),
-        name: "Greek",
-        type: "bar",
-        font: { color: "#49a81d", family: "Arial, Helvetica, sans-serif" },
-        orientation: "h"
-    };
-    var data = [trace1];
-    var layout = {
-        title: "Top Ten OTUs for Individual " +sample,
-        margin: {l: 100, r: 100, t: 100, b: 100}
-    };
-    Plotly.newPlot("bar", data, layout);  
-    });
-  }
-  
-  
   function optionChanged(newSample) {
-    updateCharts(newSample);
     updateMetadata(newSample);
+    updateCharts(newSample);
   }
   
   init();
