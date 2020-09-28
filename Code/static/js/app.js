4a03f5b8ba968b1e49874bbd72f1878d9cc4f4b4
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-09-28 14:10:54 -0400

    app.js edits

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -15,43 +15,45 @@ function init(){
     });
 }
 
-function updatemetadata(samples){
-    d3.json('data/samples.json').then((data) => {
+function updateMetadata(sample) {
+    d3.json("data/samples.json").then((data) => {
         var metadata = data.metadata;
-        var filterarray = metadata.filter(sampleobject =>sampleobject.ID == sample);
-        var result = filterarray[0];
-        var metapanel = d3.select('#sample-metadata');
-        metapanel.html("");
-        Object.entries(result).forEach(([key, value]) =>{
-            metapanel.append('h6').text(`${key.toUpperCase()}: ${value}`)
+        var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
+        var result = filterArray[0];
+        var metaPanel = d3.select("#sample-metadata");
+        metaPanel.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
         })
-
-var data = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      marker: {size: 28, color:'850000'},
-      value: result.wfreq,
-      title: 'Belly Button Washing Frequency<br> Scrubs per Week',
-      titlefont: {family: '"Palatino Linotype", "Book Antiqua", Palatino, serif'},
-      type: "indicator",
-      mode: "gauge+number"
-    }
-  ];
-
-  var layout = {
-    width: 450,
-     height: 400,
-     margin: { t: 25, r: 25, l: 25, b: 25 },
-     line: {
-     color: '600000'
-     },
-     paper_bgcolor: "#a5bdc6",
-     font: { color: "#85541d", family: "Serif" }
-   };
-
-  Plotly.newPlot("gauge", data, layout);
-  });
-}
+    
+    var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        marker: {size: 28, color:'850000'},
+        value: result.wfreq,
+        title: 'Belly Button Washing Frequency<br> Scrubs per Week',
+        titlefont: {family: '"Arial, Helvetica, sans-serif'},
+        type: "indicator",
+        mode: "gauge+number"
+      }
+    ];
+  
+    var layout = {
+      width: 450,
+       height: 400,
+       margin: { t: 25, r: 25, l: 25, b: 25 },
+       line: {
+       color: '600000'
+       },
+       paper_bgcolor: "#a5bdc6",
+       font: { color: "#85541d", family: "Arial, Helvetica, sans-serif" }
+     };
+  
+    
+    Plotly.newPlot("gauge", data, layout);
+    });
+  }
+  
 
 
 
